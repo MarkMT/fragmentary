d12a7627c7cd204391618e9d24c0db1a4d87b595
Mark Thomson <nzl216@gmail.com>
2019-04-16 10:48:12 -0500

    Suppress after_update broadcast if no changes to the saved record.

--- lib/fragmentary/publisher.rb
+++ lib/fragmentary/publisher.rb
@@ -33,7 +33,7 @@ module Fragmentary
       end
 
       def after_update_broadcast
-        broadcast(:after_update, self) self.previous_changes.any?
+        broadcast(:after_update, self) if self.previous_changes.any?
       end
 
       def after_destroy_broadcast
