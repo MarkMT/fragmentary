d6169c90d961edbef699b6e24cbaf0ebaa1948a2
Mark Thomson <mark@kimbolton>
2024-10-17 17:43:06 +1300

    Remove duplicated serializer code

--- lib/fragmentary/handler.rb
+++ lib/fragmentary/handler.rb
@@ -1,26 +1,4 @@
 module Fragmentary
-
-  class HandlerSerializer < ActiveJob::Serializers::ObjectSerializer
-
-    def serialize?(arg)
-      arg.is_a? Fragmentary::Handler
-    end
-
-    def serialize(handler)
-      super(
-        {
-        :class_name => handler.class.name,
-        :args => handler.args
-        }
-      )
-    end
-
-    def deserialize(hsh)
-      hsh[:class_name].constantize.new(hsh[:args])
-    end
-
-  end
-
   class Handler
     def self.all
       @@all
